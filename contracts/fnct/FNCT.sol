ad42d6b49387771d3812c75a931340132edfb0bc
rudo <rudoneko@gmail.com>
2022-04-04 12:34:32 +0900

    remove unused base contract(Ownable)

--- contracts/fnct/FNCT.sol
+++ contracts/fnct/FNCT.sol
@@ -1,9 +1,8 @@
 pragma solidity ^0.8.0;
 
-import "../../openzeppelin-contracts/contracts/access/Ownable.sol";
 import "../../openzeppelin-contracts/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 
-contract FNCT is ERC20PresetFixedSupply, Ownable {
+contract FNCT is ERC20PresetFixedSupply {
 
     constructor(
         uint256 initialSupply
